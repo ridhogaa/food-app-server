4b0ba6b24448571657c98d3c48dfa27e42277a79
ridhogaa <ridhoga186@gmail.com>
2024-05-12 15:17:34 +0700

    feat(merchant): set up pageable and spec for filter list

--- src/main/java/org/ergea/foodapp/controller/MerchantController.java
+++ src/main/java/org/ergea/foodapp/controller/MerchantController.java
@@ -4,6 +4,8 @@ import org.ergea.foodapp.dto.BaseResponse;
 import org.ergea.foodapp.dto.MerchantRequest;
 import org.ergea.foodapp.service.MerchantService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,8 +24,13 @@ public class MerchantController {
     }
 
     @GetMapping
-    public ResponseEntity<?> findAll(@RequestParam(required = false) Boolean isOpen) {
-        return ResponseEntity.ok(BaseResponse.success(merchantService.findAll(isOpen), "Success Get All Merchants"));
+    public ResponseEntity<?> findAll(
+            @PageableDefault(page = 0, size = 10) Pageable pageable,
+            @RequestParam(required = false) Boolean isOpen,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String location
+    ) {
+        return ResponseEntity.ok(BaseResponse.success(merchantService.findAll(isOpen, pageable, name, location), "Success Get All Merchants"));
     }
 
     @PutMapping(path = "{id}")
@@ -34,5 +41,10 @@ public class MerchantController {
     @DeleteMapping(path = "{id}")
     public ResponseEntity<?> delete(@PathVariable UUID id) {
         return ResponseEntity.ok(BaseResponse.success(merchantService.delete(id), "Success Delete Merchant"));
+    }
+
+    @GetMapping(path = "{id}")
+    public ResponseEntity<?> findById(@PathVariable UUID id) {
+        return ResponseEntity.ok(BaseResponse.success(merchantService.findById(id), "Success get detail merchant"));
     }
 }
--- src/main/java/org/ergea/foodapp/repository/MerchantRepository.java
+++ src/main/java/org/ergea/foodapp/repository/MerchantRepository.java
@@ -2,6 +2,7 @@ package org.ergea.foodapp.repository;
 
 import org.ergea.foodapp.entity.Merchant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.stereotype.Repository;
@@ -11,6 +12,5 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface MerchantRepository extends JpaRepository<Merchant, UUID> {
-    List<Merchant> findAllByIsOpen(Boolean isOpen);
+public interface MerchantRepository extends JpaRepository<Merchant, UUID>, JpaSpecificationExecutor<Merchant> {
 }
