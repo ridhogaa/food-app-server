648d6c156af85abfb530af76f7f2cf01e759f829
Ridho Gymnastiar Al Rasyid <ridhoga186@gmail.com>
2024-05-12 19:56:49 +0700

    refactor extends JpaSpecificationExecutor

--- src/main/java/org/ergea/foodapp/repository/OrderDetailRepository.java
+++ src/main/java/org/ergea/foodapp/repository/OrderDetailRepository.java
@@ -2,10 +2,11 @@ package org.ergea.foodapp.repository;
 
 import org.ergea.foodapp.entity.OrderDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface OrderDetailRepository extends JpaRepository<OrderDetail, UUID> {
+public interface OrderDetailRepository extends JpaRepository<OrderDetail, UUID>, JpaSpecificationExecutor<OrderDetail> {
 }
