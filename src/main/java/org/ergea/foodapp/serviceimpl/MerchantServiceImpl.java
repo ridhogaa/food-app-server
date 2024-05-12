c5859a5799684ec34b9e493045b6d2c35c3c2ad9
ridhogaa <ridhoga186@gmail.com>
2024-05-12 15:17:34 +0700

    feat(merchant): set up pageable and spec for filter list

--- src/main/java/org/ergea/foodapp/serviceimpl/MerchantServiceImpl.java
+++ src/main/java/org/ergea/foodapp/serviceimpl/MerchantServiceImpl.java
@@ -1,5 +1,6 @@
 package org.ergea.foodapp.serviceimpl;
 
+import jakarta.persistence.criteria.Predicate;
 import lombok.extern.slf4j.Slf4j;
 import org.ergea.foodapp.dto.MerchantRequest;
 import org.ergea.foodapp.dto.MerchantResponse;
@@ -9,6 +10,8 @@ import org.ergea.foodapp.repository.MerchantRepository;
 import org.ergea.foodapp.service.MerchantService;
 import org.ergea.foodapp.service.ValidationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -38,17 +41,22 @@ public class MerchantServiceImpl implements MerchantService {
     }
 
     @Override
-    public List<MerchantResponse> findAll(Boolean isOpen) {
+    public List<MerchantResponse> findAll(Boolean isOpen, Pageable pageable, String name, String location) {
+        Specification<Merchant> spec = ((root, query, criteriaBuilder) -> {
+            List<Predicate> predicates = new ArrayList<>();
+            if (name != null && !name.isEmpty()) {
+                predicates.add(criteriaBuilder.like(criteriaBuilder.lower(root.get("name")), "%" + name.toLowerCase() + "%"));
+            }
+            if (location != null && !location.isEmpty()) {
+                predicates.add(criteriaBuilder.like(criteriaBuilder.lower(root.get("location")), "%" + location.toLowerCase() + "%"));
+            }
+            if (isOpen != null) {
+                predicates.add(criteriaBuilder.equal(root.get("isOpen"), isOpen));
+            }
+            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+        });
         var response = new ArrayList<MerchantResponse>();
-        if (isOpen != null) {
-            merchantRepository.findAllByIsOpen(isOpen).forEach(
-                    merchant -> response.add(merchantMapper.toMerchantResponse(merchant))
-            );
-        } else {
-            merchantRepository.findAll().forEach(
-                    merchant -> response.add(merchantMapper.toMerchantResponse(merchant))
-            );
-        }
+        merchantRepository.findAll(spec, pageable).forEach(merchant -> response.add(merchantMapper.toMerchantResponse(merchant)));
         return response;
     }
 
@@ -63,6 +71,12 @@ public class MerchantServiceImpl implements MerchantService {
     public MerchantResponse delete(UUID id) {
         Merchant merchant = merchantRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "ID Merchant not found"));
         merchantRepository.delete(merchant);
+        return merchantMapper.toMerchantResponse(merchant);
+    }
+
+    @Override
+    public MerchantResponse findById(UUID id) {
+        Merchant merchant = merchantRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "ID Merchant not found"));
         return merchantMapper.toMerchantResponse(merchant);
     }
 
